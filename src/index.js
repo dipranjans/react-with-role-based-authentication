ca3d5b88604869de4d0061758c25ab67119c8be7
Diptiranjan S <15139672+diptiranjans@users.noreply.github.com>
2019-08-23 13:50:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { configureFakeBackend } from "./_helpers";
 configureFakeBackend();
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
+console.log('dipranjan testingggggg');
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
